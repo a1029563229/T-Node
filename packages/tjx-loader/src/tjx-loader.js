31c2418e889122d8adfc55b654f84cbfa7f7494c
lt <1029563229@qq.com>
2019-11-01 15:58:06 +0800

    tjx 语法树解析

--- packages/tjx-loader/src/tjx-loader.js
+++ packages/tjx-loader/src/tjx-loader.js
@@ -1,4 +1,27 @@
+class Parser {
+  constructor() {
+    this.tagOList = [];
+    this.tagCList = [];
+  }
+
+  run(htmlStr) {
+    this.matchStr(htmlStr);
+    // console.log(this.tagOList);
+    // console.log(this.tagCList);
+  }
+
+  matchStr(str) {
+    if (!str.length) return;
+    const tagORE = /(?<=<)(\w+|.*[^\w+])(?=>)/;
+    const tagCRE = /(?<=<)\/\w+(?=>)/;
+    const tagOM = str.match(tagORE);
+    const [tagStr] = tagOM;
+    
+  }
+}
+
 module.exports = function (source) {
-  console.log({ source });
+  const parser = new Parser();
+  parser.run(source)
   return source;
 }
---
+++ packages/tjx-loader/src/tjx-loader.test.js
@@ -0,0 +1,11 @@
+const tjxLoader = require('./tjx-loader');
+
+const tjxStr = `<section className='container'>
+<ul id='list'>
+  <li className='item'>item1</li>
+  <li className='item'>item2</li>
+  <li className='item'>item3</li>
+</ul>
+</section>`;
+
+console.log(tjxLoader(tjxStr));
